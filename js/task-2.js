7fce675b54f9adcd6819fabb644d5a5841e2e5ac
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2025-09-06 15:25:09 +0300

    div

--- js/task-2.js
+++ js/task-2.js
@@ -1,35 +1,31 @@
 'use strict';
 
 class Storage {
-    #items;
+  constructor(items) {
+    this.items = items;
+  }
 
-    constructor(initialItems) {
-        this.#items = initialItems;
-    }
+  getItems() {
+    return this.items;
+  }
 
-    getItems() {
-        return this.#items;
-    }
+  addItem(newItem) {
+    this.items.push(newItem);
+  }
 
-    addItem(newItem) {
-        this.#items.push(newItem);
-    }
-
-    removeItem(itemToRemove) {
-        this.#items = this.#items.filter(item => item !== itemToRemove);
-    }
+  removeItem(itemToRemove) {
+    this.items = this.items.filter(item => item !== itemToRemove);
+  }
 }
 
-
-
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
-storage.addItem("Droid");
+storage.addItem('Droid');
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 
-storage.removeItem("Prolonger");
+storage.removeItem('Prolonger');
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
-storage.removeItem("Scaner");
+storage.removeItem('Scaner');
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
--- js/task-3.js
+++ js/task-3.js
@@ -1,35 +1,32 @@
 'use strict';
 
 class StringBuilder {
-    #value;
+  constructor(value) {
+    this.value = value;
+  }
 
-    constructor(initialValue) {
-        this.#value = initialValue;
-    }
+  getValue() {
+    return this.value;
+  }
 
-    getValue() {
-        return this.#value;
-    }
+  padEnd(str) {
+    this.value += str;
+  }
 
-    padEnd(str) {
-        this.#value += str;
-    }
+  padStart(str) {
+    this.value = str + this.value;
+  }
 
-    padStart(str) {
-        this.#value = str + this.#value;
-    }
-
-    padBoth(str) {
-        this.#value = str + this.#value + str;
-    }
+  padBoth(str) {
+    this.value = str + this.value + str;
+  }
 }
 
-
-const builder = new StringBuilder(".");
+const builder = new StringBuilder('.');
 console.log(builder.getValue()); // "."
-builder.padStart("^");
+builder.padStart('^');
 console.log(builder.getValue()); // "^."
-builder.padEnd("^");
+builder.padEnd('^');
 console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
+builder.padBoth('=');
 console.log(builder.getValue()); // "=^.^="
