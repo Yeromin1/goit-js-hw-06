6112afc52292cf49a7494426b0e7f26614c4506a
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2025-11-16 17:31:33 +0200

    hg

--- src/js/questions.js
+++
@@ -1,7 +0,0 @@
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
-
-new Accordion('.acc-container', {
-  duration: 300,
-  openOnInit: [0],
-});
--- src/main.js
+++
@@ -1,4 +0,0 @@
-import './js/tariffs.js';
-import './js/questions.js';
-import './js/menu.js';
-import './js/header.js';
